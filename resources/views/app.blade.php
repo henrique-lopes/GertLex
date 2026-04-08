94aa37dc338b06f4cfd85f1dc9765fe815ae6e2f
Henrique Lopes <henrique@lexflow.com.br>
2026-04-08 17:00:56 -0300

    feat: custom scale favicon for GertLex

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,9 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'><rect width='32' height='32' rx='8' fill='%23C9A84C'/><g transform='translate(4,4) scale(1.5)' stroke='%23000' stroke-width='1.5' stroke-linecap='round' stroke-linejoin='round' fill='none'><line x1='8' y1='3' x2='8' y2='13'/><path d='M4 13h8'/><path d='M3 6l-2 4h4z'/><path d='M13 6l-2 4h4z'/><line x1='3' y1='6' x2='8' y2='4'/><line x1='13' y1='6' x2='8' y2='4'/></g></svg>">
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
